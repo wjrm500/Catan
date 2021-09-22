95bc6646b910293aa8cd3d8aa80786742c92eeb5
William May <wjrm500@gmail.com>
2021-09-22 09:52:16 +0800

    Further improvements inc. making sure ovals sit atop hexagons

--- frontend/Tkinter.py
+++ frontend/Tkinter.py
@@ -10,8 +10,7 @@ class TkinterFrontend():
         self.canvas = tkinter.Canvas(
             self.root,
             width = initial_canvas_width,
-            height = initial_canvas_height,
-            cursor = 'X_cursor'
+            height = initial_canvas_height
         )
         self.canvas.pack(fill = "both", expand = True)
         self.tk_hexagons = [] ### Tkinter polygon objects from create_polygon
@@ -52,29 +51,52 @@ class TkinterFrontend():
         self.draw_board()
     
     def node_bubbles(self, event_origin):
-        
+        ########################
+        # Reset canvas objects #
+        ########################
 
+        ### Replace thickened hexagons with their non-thickened counterparts
         for hexagon in self.thickened_tk_hexagons:
             self.canvas.delete(self.hexagon_tag(hexagon))
             self.draw_tk_hexagon(hexagon)
+        
         self.canvas.delete('tk_oval')
+
+        ##########################################
+        # Calculate arguments for canvas objects #
+        ##########################################
 
         x1 = event_origin.x
         y1 = event_origin.y
         node_dists = [(node, math.sqrt(pow(node.real_x - x1, 2) + pow(node.real_y - y1, 2))) for node in self.game.distributor.nodes]
         min_node_dist = min(map(lambda x: x[1], node_dists))
+        draw_tk_hexagon_args_list = [] ### Necessary to construct argument lists and then loop through creation separately to ensure ovals sit atop hexagons
+        draw_tk_oval_args_list = []
         for node, dist in node_dists:
             closest_to_cursor = dist == min_node_dist
             reversed_dist = max(self.scale - dist, 0)
             if closest_to_cursor:
                 self.thickened_tk_hexagons = [hexagon for hexagon in node.hexagons]
                 for hexagon in self.thickened_tk_hexagons:
-                    self.draw_tk_hexagon(hexagon, lw_factor = reversed_dist / self.scale)
+                    draw_tk_hexagon_args_list.append({'hexagon': hexagon, 'lw_factor': reversed_dist / self.scale})
             if reversed_dist > 0:
                 circle_radius = reversed_dist / 5
-                fill_color = 'limegreen' if closest_to_cursor else 'white'
-                tk_oval = self.canvas.create_oval(node.real_x - circle_radius, node.real_y - circle_radius, node.real_x + circle_radius, node.real_y + circle_radius, tags = 'tk_oval', fill = fill_color, width = reversed_dist / 10)
-                self.tk_ovals.append(tk_oval)
+                fill_color = 'limegreen' if dist / self.scale < 0.2 else 'white'
+                line_width = reversed_dist / 10
+                draw_tk_oval_args_list.append({'node': node, 'circle_radius': circle_radius, 'fill': fill_color, 'width': line_width})
+        
+        ################################
+        # Actually draw canvas objects #
+        ################################
+
+        for args in draw_tk_hexagon_args_list:
+            self.draw_tk_hexagon(args['hexagon'], lw_factor = args['lw_factor'])
+
+        for args in draw_tk_oval_args_list:
+            self.draw_tk_oval(args['node'], args['circle_radius'], fill = args['fill'], width = args['width'])
+        
+        cursor = 'hand2' if min_node_dist / self.scale < 0.2 else ''
+        self.canvas.config(cursor = cursor)
             
     def draw_tk_hexagon(self, hexagon, lw_factor = 0):
         hexagon_tag = self.hexagon_tag(hexagon)
@@ -84,6 +106,10 @@ class TkinterFrontend():
         points = [item for sublist in points for item in sublist]
         tk_hexagon = self.canvas.create_polygon(points, fill = '', outline = 'black', tags = ['tk_hexagon', hexagon_tag], width = 5 * lw_factor if lw_factor else 1)
         self.tk_hexagons.append(tk_hexagon)
+
+    def draw_tk_oval(self, node, circle_radius, fill = 'white', width = 1):
+        tk_oval = self.canvas.create_oval(node.real_x - circle_radius, node.real_y - circle_radius, node.real_x + circle_radius, node.real_y + circle_radius, tags = 'tk_oval', fill = fill, width = width)
+        self.tk_ovals.append(tk_oval)
     
     def hexagon_tag(self, hexagon):
         return 'tk_hexagon_' + str(hexagon.id)
