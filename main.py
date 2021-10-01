a16aa15177477f59d2bf847ea48c40c7731f9c14
William May <wjrm500@gmail.com>
2021-10-01 17:02:42 +0800

    Moving canvas to left hand side and having home window leading into main loop

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from catan.mechanics.Game import Game
 from config import config
 from frontend.Matplotlib import Matplotlib
-from frontend.Tkinter import TkinterFrontend
+from frontend.Tkinter.Home import Home
 
-game = Game(config, ['Will', 'Kate'])
+game = Game(config, ['Will', 'Kate'], 100)
 game.setup_board()
 game.setup_cards()
 game.setup_movable_pieces()
-# game.start()
 # Matplotlib.draw_board(game)
-tk = TkinterFrontend(game)
+tk = Home()
 tk.run()
