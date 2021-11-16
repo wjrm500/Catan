3e290d15022bd88dc750ba8bcfe6dac34a84abe4
William May <wjrm500@gmail.com>
2021-11-16 22:24:16 +0800

    Finishing layout for settling phase UI

--- frontend/Tkinter/phases/primary/SettlingPhase.py
+++ frontend/Tkinter/phases/primary/SettlingPhase.py
@@ -1,52 +1,80 @@
 import tkinter
+import tkinter.scrolledtext
+import textwrap
+from frontend.ColorUtils import ColorUtils
 from frontend.Tkinter.phases.Phase import Phase
 from frontend.Tkinter.rendering.HexagonRendering import HexagonRendering
+from functools import partial
 
 class SettlingPhase(Phase):
     def __init__(self, chaperone):
         super().__init__(chaperone)
         self.root.geometry('1000x500')
         self.frames = []
-        self.inner_frame = tkinter.Frame(self.root, background = '#dddddd')
+        standard_frame = partial(tkinter.Frame, background = self.BG_COLOR)#, highlightbackground = ColorUtils.darken_hex(self.BG_COLOR, 0.03), highlightthickness = 1)
+        self.inner_frame = standard_frame(self.root)
         self.inner_frame.pack(fill = 'both', expand = True, padx = 10, pady = 10)
-        inner_frame_top = tkinter.Frame(self.inner_frame, background = '#cccccc', height = 50)
+        inner_frame_top = standard_frame(self.inner_frame, height = 50)
         inner_frame_top.pack(side = tkinter.TOP, fill = 'x')
-        inner_frame_top_left = tkinter.Frame(inner_frame_top, background = '#bbbbbb', height = 50)
-        inner_frame_top_right = tkinter.Frame(inner_frame_top, background = '#aaaaaa', height = 50)
+        inner_frame_top_left = standard_frame(inner_frame_top, height = 50)
+        inner_frame_top_right = standard_frame(inner_frame_top, height = 50)
         inner_frame_top_left.place(in_ = inner_frame_top, anchor = 'e', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
         inner_frame_top_right.place(in_ = inner_frame_top, anchor = 'w', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
-        inner_frame_middle = tkinter.Frame(self.inner_frame, background = '#999999')
+        inner_frame_middle = standard_frame(self.inner_frame)
         inner_frame_middle.pack(side = tkinter.TOP, fill = 'both', expand = True)
-        inner_frame_middle_left = tkinter.Frame(inner_frame_middle, background = '#888888')
-        self.inner_frame_middle_right = tkinter.Frame(inner_frame_middle, background = '#777777')
+        inner_frame_middle_left = standard_frame(inner_frame_middle)
+        inner_frame_middle_right = standard_frame(inner_frame_middle)
         inner_frame_middle_left.place(in_ = inner_frame_middle, anchor = 'e', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
-        self.inner_frame_middle_right.place(in_ = inner_frame_middle, anchor = 'w', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
-        # self.canvas = tkinter.Canvas(inner_frame_middle_left, background = 'lightblue', bd = 0, highlightthickness = 0)
-        # self.canvas.pack(expand = True)
-        # self.hexagon_rendering = HexagonRendering(self)
-
-        inner_frame_bottom = tkinter.Frame(self.inner_frame, background = '#cccccc', height = 50)
+        inner_frame_middle_right.place(in_ = inner_frame_middle, anchor = 'w', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
+        inner_frame_bottom = standard_frame(self.inner_frame, height = 50)
         inner_frame_bottom.pack(side = tkinter.BOTTOM, fill = 'x')
-        inner_frame_bottom_left = tkinter.Frame(inner_frame_bottom, background = '#bbbbbb', height = 50)
-        inner_frame_bottom_right = tkinter.Frame(inner_frame_bottom, background = '#aaaaaa', height = 50)
+        inner_frame_bottom_left = standard_frame(inner_frame_bottom, height = 50)
+        inner_frame_bottom_right = standard_frame(inner_frame_bottom, height = 50, padx = 10)
         inner_frame_bottom_left.place(in_ = inner_frame_bottom, anchor = 'e', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
         inner_frame_bottom_right.place(in_ = inner_frame_bottom, anchor = 'w', relheight = 1.0, relwidth = 0.5, relx = 0.5, rely = 0.5)
+        
+        self.canvas = tkinter.Canvas(inner_frame_middle_left, background = 'lightblue', bd = 0, highlightthickness = 0)
+        self.canvas.pack(expand = True)
+        self.hexagon_rendering = HexagonRendering(self)
 
-        # self.catan_logo_canvas = self.render_catan_logo_canvas(where = inner_frame_top_right, width = 1.0, height = 0.5)
-        # self.catan_logo_canvas.pack(side = tkinter.LEFT, pady = 5)
-        block_under_title = tkinter.Frame(inner_frame_top_right, background = 'black', height = 5)
+        block_under_title = tkinter.Frame(inner_frame_top_right, background = 'black', height = 5, bd = 0, highlightthickness = 0)
         block_under_title.pack(side = tkinter.BOTTOM, fill = 'x')
-        label = tkinter.Label(inner_frame_top_right, text = 'CATAN', font = ('Arial', 20, 'bold'))
-        label.pack(side = tkinter.LEFT)
-        label = tkinter.Label(inner_frame_top_right, text = 'SETTLING PHASE', font = ('Arial', 12), padx = 10)
-        label.pack(side = tkinter.LEFT)
+        label = tkinter.Label(inner_frame_top_right, text = 'CATAN', font = ('Arial', 20, 'bold'), background = self.BG_COLOR)
+        label.pack(anchor = tkinter.S, side = tkinter.LEFT)
+        label = tkinter.Label(inner_frame_top_right, text = 'SETTLING PHASE', font = ('Arial', 12), padx = 5, pady = 5, background = self.BG_COLOR)
+        label.pack(anchor = tkinter.S, side = tkinter.LEFT)
         
-        # label.place(anchor = 'e', relwidth = 1.0, relx = 1.0, rely = 0.6)
+        ### Specific to settling phase (the rest isn't)
+        self.text_area = tkinter.scrolledtext.ScrolledText(inner_frame_middle_right, font = ('Arial', 12), padx = 10, wrap = 'word')
+        self.text_area.pack(padx = 10, pady = 10)
+        self.text_area.config(state = 'normal')
+        self.text_area.insert('end', self.get_introductory_text())
+        self.text_area.yview('end')
+        self.text_area.config(state = 'disabled')
         
-        
+        self.instruction_text = tkinter.StringVar()
+        self.instruction_text.set('Awaiting instruction...')
+        self.instruction = tkinter.Label(inner_frame_bottom_left, textvariable = self.instruction_text)
+        self.instruction.place(anchor = tkinter.CENTER, relheight = 0.5, relwidth = 0.8, relx = 0.5, rely = 0.5)
+
+        self.button_text = tkinter.StringVar()
+        self.button_text.set('N/A')
+        self.button = tkinter.Button(inner_frame_bottom_right, textvariable = self.button_text)
+        self.button.place(anchor = tkinter.W, relheight = 0.5, relwidth = 0.5, relx = 0.5, rely = 0.5)
     
     def run(self):
-        # self.root.bind('<Configure>', self.hexagon_rendering.handle_resize)
-        # self.canvas.bind('<Motion>', lambda evt: self.hexagon_rendering.handle_motion(evt))
-        # self.canvas.bind('<Leave>', self.hexagon_rendering.unfocus_focused_hexagons)
+        self.root.bind('<Configure>', self.hexagon_rendering.handle_resize)
+        self.canvas.bind('<Motion>', lambda evt: self.hexagon_rendering.handle_motion(evt))
+        self.canvas.bind('<Leave>', self.hexagon_rendering.unfocus_focused_hexagons)
         self.root.mainloop()
+    
+    def get_introductory_text(self):
+        return textwrap.dedent("""
+            Welcome to Catan!
+
+            A game of Catan begins with each player placing settlements on two nodes, with a single road leading away from each settlement.
+
+            Players take it in turns to place settlements and roads, with turn-taking following the “snake draft” format, such that the player who settles first will be the player who settles last.
+
+            Players are ordered randomly for the first round of settling.
+        """)
