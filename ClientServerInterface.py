1620b3ca30173fcecc7edb1f302d8dd93841102c
William May <wjrm500@gmail.com>
2021-11-23 09:31:47 +0800

    Fixing nested object serialization problem by making use of __getstate__ dunder method

--- ClientServerInterface.py
+++ ClientServerInterface.py
@@ -8,11 +8,7 @@ class ClientServerInterface:
         try:
             encoded_message = json.dumps(message).encode('utf-8')
         except:
-            try:
-                encoded_message = pickle.dumps(message)
-            except:
-                serializable_message = self.get_serializable_message(message)
-                encoded_message = pickle.dumps(serializable_message)
+            encoded_message = pickle.dumps(message)
         bytes_to_send = str(asizeof(encoded_message)).encode('utf-8')
         recipient.send(bytes_to_send) ### Header
         recipient.send(encoded_message)
@@ -29,30 +25,3 @@ class ClientServerInterface:
             except:
                 data = pickle.loads(data)
         return data
-    
-    def get_serializable_message(self, message):
-        ### The first custom recursive function I've built that actually works!!!
-        ### Removes any unserializable properties from any objects in the message
-        if isinstance(message, dict):
-            to_return = {}
-            for key, value in message.items():
-                if not isinstance(value, list) and not isinstance(value, dict):
-                    if isinstance(value, object):
-                        if hasattr(value, 'get_serializable_copy') and callable(getattr(value, 'get_serializable_copy')):
-                            to_return[key] = value.get_serializable_copy()
-                        else:
-                            to_return[key] = value
-                else:
-                    to_return[key] = self.get_serializable_message(value)
-        elif isinstance(message, list):
-            to_return = []
-            for value in message:
-                if not isinstance(value, list) and not isinstance(value, dict):
-                    if isinstance(value, object):
-                        if hasattr(value, 'get_serializable_copy') and callable(getattr(value, 'get_serializable_copy')):
-                            to_return.append(value.get_serializable_copy())
-                        else:
-                            to_return.append(value)
-                else:
-                    to_return.append(self.get_serializable_message(value))
-        return to_return
--- backend/Unserializable.py
+++ backend/Unserializable.py
@@ -1,17 +1,12 @@
 import abc
-import copy
+from copy import deepcopy
 
 class Unserializable(abc.ABC):
     @abc.abstractmethod
     def unserializable_properties(self):
         pass
-
-    def get_serializable_copy(self):
-        saved_properties = {}
-        for unserializable_property in self.unserializable_properties():
-            saved_properties[unserializable_property] = getattr(self, unserializable_property)
-            delattr(self, unserializable_property)
-        self_copy = copy.deepcopy(self)
-        for key, value in saved_properties.items():
-            setattr(self, key, value)
-        return self_copy
+    
+    def __getstate__(self):
+        d = self.__dict__
+        unserializable_properties = self.unserializable_properties()
+        return {k: v for k, v in d.items() if k not in unserializable_properties}
