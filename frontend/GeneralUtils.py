67cfb0989330dc2734b3d1c3d63696710eeacf52
William May <wjrm500@gmail.com>
2021-12-01 21:26:55 +0800

    Better ordinal numbers for settling phase printout

--- frontend/GeneralUtils.py
+++ frontend/GeneralUtils.py
@@ -1,11 +1,23 @@
+import inflect
+
 class GeneralUtils:
     @staticmethod
     def filter_dict(dictionary, keys):
         return {k: v for k, v in dictionary.items() if k in keys}
     
+    # @staticmethod
+    # def get_ordinal(n):
+    #     return "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4]) ### https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+
     @staticmethod
     def get_ordinal(n):
-        return "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4]) ### https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+        if n >= 1 and n <= 4:
+            d = {1: 'first', 2: 'second', 3: 'third', 4: 'fourth'}
+            return d[n]
+        else:
+            p = inflect.engine()
+            return p.number_to_words(p.ordinal(n))
+            
     
     @staticmethod
     def safe_isinstance(obj: object, type: str) -> bool:
