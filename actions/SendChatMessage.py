af474abe6f27238a6c3f009b8a533297bdd2c34a
William May <wjrm500@gmail.com>
2022-01-01 18:24:43 +0800

    Completing work on chat frame

--- actions/SendChatMessage.py
+++ actions/SendChatMessage.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from actions.Action import Action
 
 class SendChatMessage(Action):
@@ -11,8 +12,15 @@ class SendChatMessage(Action):
         self.hexagon_rendering = chaperone.current_phase.hexagon_rendering
         self.reload_all_players()
         self.update_gui()
+        self.game_phase.notebook_frame_handlers['chat'].text_inserted = True
 
     def update_gui(self):
         text_area = self.get_chat_text_area()
-        text = f'\n\n{self.data["player"].name}: {self.data["message"]}'
-        self.text_insert(text_area, text, 'purple_font')
+        prefix = '\n' if self.game_phase.notebook_frame_handlers['chat'].text_inserted else ''
+        self.text_insert(text_area, prefix)
+        datetime_text = f'[{datetime.now().strftime("%H:%M:%S")}] '
+        self.text_insert(text_area, datetime_text, 'datetime_font')
+        name_text = self.data['player'].name
+        self.text_insert(text_area, name_text, f'{self.data["player"].name}_font')
+        message_text = f': {self.data["message"]}'
+        self.text_insert(text_area, message_text)
--- frontend/Tkinter/phases/game/sub_phases/notebook_frame_handlers/ChatFrameHandler.py
+++ frontend/Tkinter/phases/game/sub_phases/notebook_frame_handlers/ChatFrameHandler.py
@@ -6,13 +6,24 @@ from frontend.Tkinter.phases.Phase import Phase
 from frontend.Tkinter.phases.game.sub_phases.notebook_frame_handlers.BaseFrameHandler import BaseFrameHandler
 
 class ChatFrameHandler(BaseFrameHandler):
+    def __init__(self, phase, notebook) -> None:
+        super().__init__(phase, notebook)
+        self.text_inserted = False
+
     def setup(self):
         self.text_area = tkinter.scrolledtext.ScrolledText(self.frame, font = ('Arial', 12), padx = 10, pady = 10, wrap = 'word', background = ColorUtils.lighten_hex(Phase.BG_COLOR, 0.2))
         self.text_area.config(state = 'disabled')
-        self.text_area.pack(expand = True, fill = 'both', padx = 5, pady = 5)
-        self.text_entry = EntryWithPlaceholder(self.frame, placeholder = 'Type message here and hit enter to send!')
-        self.text_entry.pack(expand = True, fill = 'both', padx = 5, pady = 5)
+        self.frame.grid_columnconfigure(0, weight = 1)
+        self.text_area.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = 'ew')
+        self.frame.grid_rowconfigure(0, weight = 1, uniform = 'catan')
+        self.text_entry = EntryWithPlaceholder(self.frame, placeholder = 'Type your message here and hit enter to send!', font = ('Arial', 12))
+        self.text_entry.grid(row = 1, column = 0, sticky = 'ew', padx = 5, pady = 5, ipadx = 3, ipady = 3)
+        self.frame.grid_rowconfigure(1, weight = 1)
         self.text_entry.bind('<Return>', self.send_chat_message)
+
+        for player in self.player.game.players:
+            self.text_area.tag_config(f'{player.name}_font', foreground = player.color, font = ('Arial', 12, 'bold'))
+        self.text_area.tag_config('datetime_font', foreground = 'gray', font = ('Arial', 12))
     
     def send_chat_message(self, event):
         text = self.text_entry.get()
