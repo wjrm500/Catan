cb799f40d483bdb4e0174dd64c9fce5831ca877a
William May <wjrm500@gmail.com>
2022-01-07 17:28:36 +0800

    Running GUI in main thread for MacOS compatibility

--- client.py
+++ client.py
@@ -22,9 +22,10 @@ class Client:
         self.socket.connect((self.host, self.port))
         self.queue = multiprocessing.Queue()
         self.receive_thread = threading.Thread(target = self.receive)
-        self.gui_thread = threading.Thread(target = self.gui)
+        # self.gui_thread = threading.Thread(target = self.gui)
         self.receive_thread.start()
-        self.gui_thread.start()
+        self.gui()
+        # self.gui_thread.start()
     
     def receive(self):
         while True:
